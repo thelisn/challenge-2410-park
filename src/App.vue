6df6e2a659f4761b99aea4ae8c346d559f540f78
lmotp <lmotp@daum.net>
2024-04-12 10:31:33 +0900

    refactor : 메인에서 헤더 분리

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import TheHeader from "@/layouts/TheHeader.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <main id="main">
-    <header>
-      <div class="wrapper">
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-      </div>
-    </header>
-
+    <TheHeader />
     <RouterView />
   </main>
 </template>
@@ -26,16 +19,5 @@ import { RouterLink, RouterView } from "vue-router";
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
-  & header {
-    margin-bottom: 10px;
-
-    & nav {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      gap: 20px;
-    }
-  }
 }
 </style>
---
+++ src/layouts/TheHeader.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style>
+header {
+  margin-bottom: 10px;
+
+  & nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+
+    .router-link-active {
+      background-color: red;
+    }
+  }
+}
+</style>
